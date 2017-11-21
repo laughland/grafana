83010be2dcf8b85dfbc9fde127dda3667e9c005b
andrew_laughland <andrew_laughland@troweprice.com>
2017-11-21 08:45:18 -0500

    Adds Minio support for storing notifiaction images

--- pkg/components/imguploader/imguploader_test.go
+++ pkg/components/imguploader/imguploader_test.go
@@ -10,6 +10,33 @@ import (
 
 func TestImageUploaderFactory(t *testing.T) {
 	Convey("Can create image uploader for ", t, func() {
+		Convey("MINIO uploader", func() {
+            var err error
+
+            setting.NewConfigContext(&setting.CommandLineArgs{
+            	HomePath: "../../../",
+            })
+
+            setting.ImageUploadProvider = "minio"
+
+            minioSec, err := setting.Cfg.GetSection("external_image_storage.minio")
+            minioSec.NewKey("endpoint", "localhost:9000")
+            minioSec.NewKey("bucketName", "alerts")
+            minioSec.NewKey("accessKeyID", "admin")
+            minioSec.NewKey("secretAccessKey", "password")
+
+            uploader, err := NewImageUploader()
+
+            So(err, ShouldBeNil)
+            original, ok := uploader.(*MinioUploader)
+
+            So(ok, ShouldBeTrue)
+            So(original.endpoint, ShouldEqual, "localhost:9000")
+            So(original.bucketName, ShouldEqual, "alerts")
+            So(original.accessKeyID, ShouldEqual, "admin")
+            So(original.secretAccessKey, ShouldEqual, "password")
+        })
+
 		Convey("S3ImageUploader config", func() {
 			setting.NewConfigContext(&setting.CommandLineArgs{
 				HomePath: "../../../",
---
+++ pkg/components/imguploader/miniouploader.go
@@ -0,0 +1,68 @@
+package imguploader
+
+import (
+	"context"
+	"fmt"
+	"log"
+    "net/url"
+    "time"
+
+	"github.com/minio/minio-go"
+	//"github.com/grafana/grafana/pkg/log"
+	"github.com/grafana/grafana/pkg/util"
+)
+
+type MinioUploader struct {
+	endpoint        string
+	bucketName      string
+	accessKeyID     string
+	secretAccessKey string
+	useSSL          bool
+	log             log.Logger
+}
+
+func NewMinioUploader(endpoint string, bucketName string, accessKeyID string, secretAccessKey string, useSSL bool) *MinioUploader {
+	return &MinioUploader{
+		endpoint:        endpoint,
+		bucketName:      bucketName,
+		accessKeyID:     accessKeyID,
+		secretAccessKey: secretAccessKey,
+		useSSL:          useSSL,
+	}
+}
+
+func (u *MinioUploader) Upload(ctx context.Context, imageDiskPath string) (string, error) {
+
+    // Initialize minio client object.
+    minioClient, err := minio.New(u.endpoint, u.accessKeyID, u.secretAccessKey, u.useSSL)
+
+    if err != nil {
+       log.Fatalln(err)
+    }
+
+    //create random name for image
+    objectName := util.GetRandomString(20) + ".png"
+    contentType := "image/png"
+
+    // Upload the image file with FPutObject
+    n, err := minioClient.FPutObject(u.bucketName, objectName, imageDiskPath, minio.PutObjectOptions{ContentType:contentType})
+    log.Printf("Successfully uploaded %s of size %d\n", objectName, n)
+
+    if err != nil {
+      log.Fatalln(err)
+    }
+
+    // Set request parameters for content-disposition.
+    reqParams := make(url.Values)
+    reqParams.Set("response-content-disposition", "attachment; filename=\"objectName\"")
+
+    // Generates a presigned url which expires in a day.
+    presignedURL, err := minioClient.PresignedGetObject(u.bucketName, objectName, time.Second * 24 * 60 * 60 * 7, reqParams)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    return presignedURL.String(), nil
+}
+
+
